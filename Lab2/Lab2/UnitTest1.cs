2d11228a51dc67c56d14e02a6698c2ba73455b32
God0nlyKnows <37994631+God0nlyKnows@users.noreply.github.com>
2023-03-16 12:09:39 +0100

    lab2

---
+++ Lab2/Lab2/UnitTest1.cs
@@ -0,0 +1,213 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace Lab2
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            EdgeOptions options = new EdgeOptions();
+            options.AddArguments("start-maximized");
+
+            driver = new EdgeDriver(options);
+        }
+
+        [Test]
+        public void TestLoginWithValidCredentials()
+        {
+            // Navigate to the login page
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+            // Enter valid credentials
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+
+            // Click the login button
+            driver.FindElement(By.Id("login-button")).Click();
+
+            // Verify that the user is logged in successfully
+            Assert.AreEqual("https://www.saucedemo.com/inventory.html", driver.Url);
+        }
+
+        [Test]
+        public void TestLoginWithInvalidCredentials()
+        {
+            // Navigate to the login page
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+            // Enter invalid credentials
+            driver.FindElement(By.Id("user-name")).SendKeys("invalid_user");
+            driver.FindElement(By.Id("password")).SendKeys("invalid_password");
+
+            // Click the login button
+            driver.FindElement(By.Id("login-button")).Click();
+
+            // Verify that the user is not logged in and an error message is displayed
+            Assert.IsTrue(driver.FindElement(By.CssSelector("h3[data-test='error']")).Displayed);
+        }
+        [Test]
+        public void TestSortingFunctionality()
+        {
+            // Login with valid credentials
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            // Navigate to the Products page
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/inventory.html");
+
+
+            // Verify that the products are sorted correctly
+            var productPrices = driver.FindElements(By.CssSelector(".inventory_item_price"));
+            for (int i = 1; i < productPrices.Count; i++)
+            {
+                var previousPrice = double.Parse(productPrices[i - 1].Text.Substring(1));
+                var currentPrice = double.Parse(productPrices[i].Text.Substring(1));
+                Assert.IsTrue(previousPrice <= currentPrice);
+            }
+        }
+
+        [Test]
+        public void TestAddProductToCart()
+        {
+            // Login with valid credentials
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            // Navigate to the Products page
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/inventory.html");
+
+            // Add the first product to the cart
+            driver.FindElement(By.CssSelector(".btn_primary")).Click();
+
+            // Verify that the cart badge is updated to 1
+            var cartBadge = driver.FindElement(By.CssSelector(".shopping_cart_badge"));
+            Assert.AreEqual("1", cartBadge.Text);
+        }
+
+        [Test]
+        public void TestRemoveProductFromCart()
+        {
+            // Login with valid credentials
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            // Navigate to the Products page
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/inventory.html");
+
+            // Add the first product to the cart
+            driver.FindElement(By.CssSelector(".btn_primary")).Click();
+
+            // Navigate to the Cart page
+            driver.FindElement(By.CssSelector(".shopping_cart_link")).Click();
+
+            // Remove the product from the cart
+            driver.FindElement(By.CssSelector(".cart_item")).FindElement(By.CssSelector(".btn_secondary")).Click();
+
+            // Verify that the cart is empty
+            Assert.IsTrue(driver.FindElement(By.CssSelector("div[class='cart_list']")).Text.Contains("Your cart is empty"));
+        }
+
+        [Test]
+        public void TestCheckoutWithOneItem()
+        {
+            // Login with valid credentials
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            // Navigate to the Products page
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/inventory.html");
+
+            // Add the first product to the cart
+            driver.FindElement(By.CssSelector(".btn_primary")).Click();
+
+            // Navigate to the Cart page
+            driver.FindElement(By.CssSelector(".shopping_cart_link")).Click();
+
+            // Click the Checkout button
+            driver.FindElement(By.CssSelector(".checkout_button")).Click();
+
+            // Enter the checkout information
+            driver.FindElement(By.Id("first-name")).SendKeys("John");
+            driver.FindElement(By.Id("last-name")).SendKeys("Doe");
+            driver.FindElement(By.Id("postal-code")).SendKeys("12345");
+
+            // Click the Continue button
+            driver.FindElement(By.CssSelector(".cart_button")).Click();
+
+            // Verify that the user is on the Checkout: Overview page
+            Assert.AreEqual("https://www.saucedemo.com/checkout-step-two.html", driver.Url);
+        }
+
+        [Test]
+        public void TestCompleteCheckoutProcess()
+        {
+            // Login with valid credentials
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            // Navigate to the Products page
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/inventory.html");
+
+            // Add the first product to the cart
+            driver.FindElement(By.CssSelector(".btn_primary")).Click();
+
+            // Click the cart icon to go to the cart page
+            driver.FindElement(By.CssSelector(".shopping_cart_link")).Click();
+
+            // Click the Checkout button
+            driver.FindElement(By.CssSelector(".btn_action.checkout_button")).Click();
+
+            // Enter checkout information
+            driver.FindElement(By.Id("first-name")).SendKeys("John");
+            driver.FindElement(By.Id("last-name")).SendKeys("Doe");
+            driver.FindElement(By.Id("postal-code")).SendKeys("12345");
+
+            // Click the Continue button
+            driver.FindElement(By.CssSelector(".btn_primary.cart_button")).Click();
+
+            // Verify that the correct items are in the cart
+            var items = driver.FindElements(By.CssSelector(".cart_item"));
+            Assert.AreEqual(1, items.Count);
+            Assert.AreEqual("Sauce Labs Backpack", items[0].FindElement(By.CssSelector(".cart_item_label a")).Text);
+
+            // Click the Finish button
+            driver.FindElement(By.CssSelector(".btn_action.cart_button")).Click();
+
+            // Verify that the checkout complete message is displayed
+            Assert.AreEqual("https://www.saucedemo.com/checkout-complete.html", driver.Url);
+            Assert.IsTrue(driver.FindElement(By.CssSelector(".complete-header")).Displayed);
+        }
+
+        [Test]
+        public void TestLogout()
+        {
+            // Login with valid credentials
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            // Click the menu icon
+            driver.FindElement(By.CssSelector(".bm-burger-button")).Click();
+
+            // Click the Logout button
+            driver.FindElement(By.Id("logout_sidebar_link")).Click();
+
+            // Verify that the user is logged out and on the login page
+            Assert.AreEqual("https://www.saucedemo.com/", driver.Url);
+        }
+    }
+}
