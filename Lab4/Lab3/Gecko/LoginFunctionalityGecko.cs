67f5f91304b550822701ed36069d8ad6cea52246
God0nlyKnows <37994631+God0nlyKnows@users.noreply.github.com>
2023-03-30 13:36:47 +0200

    lab4

---
+++ Lab4/Lab3/Gecko/LoginFunctionalityGecko.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class LoginFunctionalityGecko
+    {
+        private IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void TestLoginWithValidCredentials()
+        {
+            // Navigate to the login page
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+            // Enter valid credentials
+            driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[1]/div/div/form/div[1]/input")).SendKeys("standard_user");
+            driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[1]/div/div/form/div[2]/input")).SendKeys("secret_sauce");
+
+            // Click the login button
+            driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[1]/div/div/form/input")).Click();
+
+            // Verify that the user is logged in successfully
+            Assert.AreEqual("https://www.saucedemo.com/inventory.html", driver.Url);
+        }
+
+        [Test]
+        public void TestLoginWithInvalidCredentials()
+        {
+            // Navigate to the login page
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+            // Enter valid credentials
+            driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[1]/div/div/form/div[1]/input")).SendKeys("standard_user");
+            driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[1]/div/div/form/div[2]/input")).SendKeys("secret_sauce");
+
+            // Click the login button
+            driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[1]/div/div/form/input")).Click();
+
+            // Verify that the user is not logged in and an error message is displayed
+            Assert.IsTrue(driver.FindElement(By.CssSelector("h3[data-test='error']")).Displayed);
+        }
+    }
+}
---
+++ Lab4/Lab3/Gecko/ProductFunctionalityGecko.cs
@@ -0,0 +1,89 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class ProductFunctionalityGecko
+    {
+        private IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void TestSortingFunctionality()
+        {
+            // Login with valid credentials
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            // Navigate to the Products page
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/inventory.html");
+
+
+            // Verify that the products are sorted correctly
+            var productPrices = driver.FindElements(By.CssSelector(".inventory_item_price"));
+            for (int i = 1; i < productPrices.Count; i++)
+            {
+                var previousPrice = double.Parse(productPrices[i - 1].Text.Substring(1));
+                var currentPrice = double.Parse(productPrices[i].Text.Substring(1));
+                Assert.IsTrue(previousPrice <= currentPrice);
+            }
+        }
+
+        [Test]
+        public void TestAddProductToCart()
+        {
+            // Login with valid credentials
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            // Navigate to the Products page
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/inventory.html");
+
+            // Add the first product to the cart
+            driver.FindElement(By.CssSelector(".btn_primary")).Click();
+
+            // Verify that the cart badge is updated to 1
+            var cartBadge = driver.FindElement(By.CssSelector(".shopping_cart_badge"));
+            Assert.AreEqual("1", cartBadge.Text);
+        }
+
+        [Test]
+        public void TestRemoveProductFromCart()
+        {
+            // Login with valid credentials
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            // Navigate to the Products page
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/inventory.html");
+
+            // Add the first product to the cart
+            driver.FindElement(By.CssSelector(".btn_primary")).Click();
+
+            // Navigate to the Cart page
+            driver.FindElement(By.CssSelector(".shopping_cart_link")).Click();
+
+            // Remove the product from the cart
+            driver.FindElement(By.CssSelector(".cart_item")).FindElement(By.CssSelector(".btn_secondary")).Click();
+
+            // Verify that the cart is empty
+            Assert.IsTrue(driver.FindElement(By.CssSelector("div[class='cart_list']")).Text.Contains("Your cart is empty"));
+        }
+    }
+}
